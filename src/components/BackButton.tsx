1a55dc158e835726e8d71b8d49f3ea7a63645b9a
azerty29242 <94782485+azerty29242@users.noreply.github.com>
2024-03-24 22:07:09 +0100

    comment old code

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,26 +1,26 @@
-interface Props {
-  onClick: () => void;
-}
+// interface Props {
+//   onClick: () => void;
+// }
 
-const BackButton = ({ onClick }: Props) => {
-  return (
-    <button className="btn btn-link icon-link mb-2 p-0" onClick={onClick}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        fill="currentColor"
-        className="bi bi-arrow-left"
-        viewBox="0 0 16 16"
-      >
-        <path
-          fillRule="evenodd"
-          d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"
-        />
-      </svg>
-      Retour
-    </button>
-  );
-};
+// const BackButton = ({ onClick }: Props) => {
+//   return (
+//     <button className="btn btn-link icon-link mb-2 p-0" onClick={onClick}>
+//       <svg
+//         xmlns="http://www.w3.org/2000/svg"
+//         width="16"
+//         height="16"
+//         fill="currentColor"
+//         className="bi bi-arrow-left"
+//         viewBox="0 0 16 16"
+//       >
+//         <path
+//           fillRule="evenodd"
+//           d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"
+//         />
+//       </svg>
+//       Retour
+//     </button>
+//   );
+// };
 
-export default BackButton;
+// export default BackButton;
